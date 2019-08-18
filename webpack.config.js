6c8a4516f8ce5c9c662fb3b88f2edc0bfc5dc864
Eoin O'Toole <eoin.otoole@pushbackmedia.co.uk>
2019-08-18 22:55:42 +0100

    created simple design system, initial landing page build

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const isDev = process.env.NODE_ENV === "development";
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: [MiniCSSExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".scss"]
+  },
+  plugins: [new MiniCSSExtractPlugin()],
+  watch: true
+};
