d4d4823942f42837cf17768b43748014aa96c425
Eoin O'Toole <eoin.otoole@corp.badoo.com>
2021-10-03 15:36:33 +0100

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,41 @@ const path = require("path");
 const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
 const isDev = process.env.NODE_ENV === "development";
 
+console.log(path.relative(__dirname, "./"));
+
+const getCSSUrlPath = (path) => {
+  if (path.match("fonts")) {
+    const directorySplit = path.split("/");
+    const file = directorySplit[directorySplit.length - 1];
+    return `/fonts/${file}`;
+  }
+
+  return path;
+};
+
 module.exports = {
   entry: "./config/webpack.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    // publicPath: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.(css|scss)$/,
-        use: [MiniCSSExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [
+          MiniCSSExtractPlugin.loader,
+          { loader: "css-loader" },
+          { loader: "resolve-url-loader", options: { sourceMap: true } },
+          { loader: "sass-loader", options: { sourceMap: true } },
+        ],
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         type: "asset/resource",
-        generator: { filename: "fonts/[name][ext]", publicPath: "/" },
+        generator: {
+          filename: "fonts/[name][ext]",
+        },
       },
       {
         test: /\.html/,
